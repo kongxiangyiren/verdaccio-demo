025ca503ebcc2c8cd429e26efa5ecedbfd018823
空巷一人 <1530688385@qq.com>
2025-11-11 18:43:02 +0800

    1

--- storage-list.mjs
+++ storage-list.mjs
@@ -1,19 +1,18 @@
-import glob from 'glob'
-import {existsSync, readFileSync, writeFileSync} from 'fs'
+import { globSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 
-const dbPath= './storage/.verdaccio-db.json'
-if(!existsSync(dbPath)){
-    console.error("请先运行 npm run start")
-    process.exit()
+const dbPath = './storage/.verdaccio-db.json';
+if (!existsSync(dbPath)) {
+  console.error('请先运行 npm run start');
+  process.exit();
 }
 
-const packages= glob.sync('./storage/**/package.json')
-const list = packages.map(pkg => pkg.replace('./storage/', '').replace('/package.json', ''));
+const packages = globSync('./storage/**/package.json');
+const list = packages.map(pkg => pkg.replace(/\\/g,"/").replace('storage/', '').replace('/package.json', ''));
 // console.log(list);
 
-
-const db= JSON.parse(readFileSync('./storage/.verdaccio-db.json',{encoding:'utf-8'})) 
+const db = JSON.parse(readFileSync('./storage/.verdaccio-db.json', { encoding: 'utf-8' }));
 // console.log(db);
-db.list=list
+db.list = list;
 
-writeFileSync('./storage/.verdaccio-db.json',JSON.stringify(db))
+writeFileSync('./storage/.verdaccio-db.json', JSON.stringify(db));
